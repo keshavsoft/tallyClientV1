bc61c2d77f5889cf1b63da93e60e0edf907ac56d
keshavsoft <nknnkumar@yahoo.com>
2026-06-16 15:35:20 +0530

    main start

---
+++ Main/v1/StockItems/end-points.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import funcFromShowAll from './ShowAll/controller.js';
+
+const tableName = "StockItems";
+const tablePath = "Data/Main/StockItems.json";
+
+const router = express.Router();
+
+router.get('/ShowAll', (req, res) => funcFromShowAll({ res, inTablePath: tablePath }));
+
+export { router };
